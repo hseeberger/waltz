664b4d9f42ff07ce0fe02488c7eb2a438f649391
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 20:45:29 +0000

    add Benchmark (cargo) benchmark result for e4055012ebc7383341076b0b6b2ab00544ae7517

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785827777071,
+  "lastUpdate": 1786481129905,
   "repoUrl": "https://github.com/hseeberger/waltz",
   "entries": {
     "Benchmark": [
@@ -59,6 +59,66 @@ window.BENCHMARK_DATA = {
             "name": "fan_out/workers/16",
             "value": 2801423,
             "range": "± 15506",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@heikoseeberger.de",
+            "name": "Heiko Seeberger",
+            "username": "hseeberger"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e4055012ebc7383341076b0b6b2ab00544ae7517",
+          "message": "Merge pull request #5 from hseeberger/dependabot/github_actions/dtolnay/rust-toolchain-6c977a6ca4077a0ceb28ffbe03f59d46e9ac8772\n\nci(deps): bump dtolnay/rust-toolchain from 2c7215f132e9ebf062739d9130488b56d53c060c to 6c977a6ca4077a0ceb28ffbe03f59d46e9ac8772",
+          "timestamp": "2026-08-11T22:43:35+02:00",
+          "tree_id": "ef8184c1fd292ef7b588f37b7978888cb50c3f0a",
+          "url": "https://github.com/hseeberger/waltz/commit/e4055012ebc7383341076b0b6b2ab00544ae7517"
+        },
+        "date": 1786481129438,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "flood/unbounded",
+            "value": 10048754,
+            "range": "± 222475",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "flood/bounded",
+            "value": 11621705,
+            "range": "± 270313",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong/pairs/1",
+            "value": 667412,
+            "range": "± 10937",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong/pairs/4",
+            "value": 802912,
+            "range": "± 3517",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fan_out/workers/4",
+            "value": 4875306,
+            "range": "± 80126",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fan_out/workers/16",
+            "value": 3496353,
+            "range": "± 19516",
             "unit": "ns/iter"
           }
         ]
